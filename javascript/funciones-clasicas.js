837a076974d75ae592ea2dddb973b8c06c3ab6db
SignLingua <signlingua@outlook.com>
2026-02-05 17:58:05 -0600

    Ejemplos JS

---
+++ javascript/funciones-clasicas.js
@@ -0,0 +1,17 @@
+function saludar(nombre) {
+  return "Hola, " + nombre;
+}
+
+console.log("Resultado", sumar(1, 2)); // sumar
+
+function sumar(a, b) {
+  return a + b;
+}
+
+function esPar(numero) {
+  return numero % 2 === 0;
+}
+
+function celsiusAFahrenheit(celsius) {
+  return celsius * 1.8 + 32;
+}
