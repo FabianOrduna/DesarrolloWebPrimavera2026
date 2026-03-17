1ae418be790aae29a34b162baf3fbcac80e4fec8
SignLingua <signlingua@outlook.com>
2026-03-17 17:45:18 -0600

    Hello world mascotas

---
+++ python/mascotas/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from utils import generar_guid
+from models import FiestaDTO, InvitadoDTO
+
+app = FastAPI()
+origins = ["*"] # Lista de orígenes permitidos para CORS, en este caso se permite cualquier origen. En un entorno de producción, es indispensable especificar los orígenes permitidos para mejorar la seguridad.
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {"message": "Bienvenido a la API de mascotas!"}
